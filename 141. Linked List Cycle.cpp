36c747168853d3ffb1b587e88f5fb5566d0d039d
Vagisha Vashista <121434180+Vagisha0022@users.noreply.github.com>
2023-09-19 23:49:06 +0530

    Create 141. Linked List Cycle.cpp

---
+++ 141. Linked List Cycle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL){
+            return false;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast->next !=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(slow == fast){
+                return true;
+
+            }
+        }
+        return false;
+        
+    }
+};
