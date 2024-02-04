49aa4808e8c334a490aac072e8617954a2b5efab
Vagisha Vashista <121434180+Vagisha0022@users.noreply.github.com>
2024-02-04 23:06:01 +0530

    Create 542. 01 Matrix.cpp

---
+++ 542. 01 Matrix.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <queue>
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> dis(n, vector<int>(m, INT_MAX)); // Initialize with a large value
+
+        queue<pair<pair<int, int>, int>> q;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    q.push({{i, j}, 0});
+                    dis[i][j] = 0; // Set distance to zero for zero elements
+                }
+            }
+        }
+
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, +1, 0, -1};
+
+        while (!q.empty()) {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol]) {
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow, ncol}, steps + 1});
+                    dis[nrow][ncol] = min(dis[nrow][ncol], steps + 1); // Update distance
+                }
+            }
+        }
+
+        return dis;
+    }
+};
