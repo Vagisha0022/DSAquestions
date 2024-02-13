954fa2aa744af214652e3ee74a5b1e667e3650f7
Vagisha Vashista <121434180+Vagisha0022@users.noreply.github.com>
2024-02-13 11:32:40 +0530

    Create 207. Course Schedule.cpp

---
+++ 207. Course Schedule.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        const int N = numCourses;
+        vector<int> adj[N];
+        for(auto it: prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> indegree(N, 0);
+        for(int i=0; i<N;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0; i<N;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
+            }
+        }
+        return topo.size() == N;
+    }
+};
